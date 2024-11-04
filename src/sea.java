f2b9ed4018797792e812614d474b7e4c5dfc45bf
splimon <splimon@hawaii.edu>
2024-11-03 19:45:54 -1000

    initialize Sea and Island classes

--- src/sea.java
+++ src/sea.java
@@ -1,5 +1,21 @@
-public class sea {
-    public static void main(String[] args) {
-        System.console().printf("E kaupē aku nō i ka hoe a kō mai\n");
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sea {
+    public static void main(String args[]) {
+    }
+}
+
+class Island {
+    String name;
+    int population;
+    Map<Island, Double> routes; // tracks travel time from one island to another
+    Map<String, Integer> resources;  // tracks resources and their quantities
+
+    public Island(String name, int population) {
+        this.name = name;
+        this.population = population;
+        this.routes = new HashMap<>();
+        this.resources = new HashMap<>();
     }
 }
